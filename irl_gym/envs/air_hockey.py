5505fe464dac8f360a49be192d0d8d2de9b574b7
rmb0034 <rmb0034@mix.wvu.edu>
2024-01-16 16:33:59 -0500

    Update to AirHockeySim

--- irl_gym/envs/air_hockey.py
+++ irl_gym/envs/air_hockey.py
@@ -8,7 +8,7 @@ class AirHockeyEnv(Env):
     def __init__(self,*,render_mode : str = "", params: dict) -> None:
         super(AirHockeyEnv,self).__init__() 
         self.time = 0 
-        self.params = params 
+        self._params = params 
         self.Ts = 1/params["freq"]
         self.bounds = params["screenSize"]
         self.timeout = params["timeout"]
@@ -29,6 +29,8 @@ class AirHockeyEnv(Env):
             self.observation_space = spaces.Box(0,1, shape=(4+4*params["numPucks"],))
         else:
             self.observation_space = spaces.Box(0,1, shape=(4+4*params["numPucks"],))
+
+        self._state = spaces.Box(0,1,shape=(4+4*params["numPucks"],))
         
         if render_mode == "human":    
             self._screen = pygame.display.set_mode(params["screenSize"],vsync=True)
@@ -39,20 +41,20 @@ class AirHockeyEnv(Env):
                         self.hitter_radius,
                         self.hitter_mass)
         self.pucks = []
-        for _ in range(self.params["numPucks"]):
+        for _ in range(self._params["numPucks"]):
             in_goal = True
             while(in_goal):
                 pose = np.random.rand(2)*(np.array(self.bounds) - 1)
                 in_goal = self.in_goal(pose)
             self.pucks.append(Puck((np.random.rand(2)*(np.array(self.bounds) - 1)),
                                     self.puck_radius, self.puck_mass))
-        self.render()
-        return self._get_obs(), {}
+        return self._get_obs(), self._get_info()
      
     def render(self):
         self._canvas = pygame.Surface(self.bounds)
         self._canvas.fill(color=(255,255,255))
         pygame.draw.rect(self._canvas,(255,0,0),(0,0,*self.bounds),width= 6)
+        print(self.goal_bounds)
         pygame.draw.rect(self._canvas,(0,0,255),self.goal_bounds)
         pygame.draw.circle(self._canvas,(255,0,0),self.hitter.pose,self.hitter.radius)
         for p in self.pucks:
@@ -61,7 +63,7 @@ class AirHockeyEnv(Env):
         if self.render_mode == "human":
             self._screen.blit(self._canvas,self._canvas.get_rect())
             pygame.display.flip()
-            pygame.time.Clock().tick(self.params["freq"])
+            pygame.time.Clock().tick(self._params["freq"])
         elif self.render_mode == "rgb_array":
             return pygame.surfarray.array3d(self._canvas)
 
@@ -73,16 +75,19 @@ class AirHockeyEnv(Env):
     def step(self, action):
         reward = -.01
         terminated = False
+        state_init = self._state
         self.move_hitter(action)
         self.update_physics()
-        for p in self.pucks:
-            if self.in_goal(p.pose) and (np.abs(p.vel) <= .01*np.ones(2,)).all():
-                reward += 1
-                terminated = True
+        obs = self._get_obs()
+        state_final = self._state
+        self.reward(state_init,action,state_final)
+       #for p in self.pucks:
+       #    if self.in_goal(p.pose) and (np.abs(p.vel) <= .01*np.ones(2,)).all():
+       #        reward += 1
+       #        terminated = True
         if self.time > self.timeout:
             terminated = True
-        self.render()
-        return self._get_obs(), reward, terminated, False, {}
+        return obs, reward, terminated, False, self._get_info()
 
     def update_physics(self):
         # Bounce off Hitter
@@ -98,7 +103,7 @@ class AirHockeyEnv(Env):
                     self.hitter.pose = self.hitter.pose + (self.hitter.radius+puck.radius-d_norm)*(d/d_norm)
 
         # Bounce off pucks
-        for  puck1, puck2 in combinations(self.pucks, 2):
+        for puck1, puck2 in combinations(self.pucks, 2):
             if np.linalg.norm(puck1.pose-puck2.pose) < puck1.radius + puck2.radius:
                total_mass = (puck1.mass+puck2.mass)
                puck1.vel = ((puck1.mass-puck2.mass)*puck1.vel + 2*puck2.mass*puck2.vel)/total_mass
@@ -109,40 +114,52 @@ class AirHockeyEnv(Env):
             bounce = ~self.in_bound(p)
             p.vel = p.vel*(2*~bounce-1)
             p.pose = np.clip(p.pose,p.radius,self.bounds-p.radius)
-            dvBounce = np.sqrt(np.abs(p.vel)) * self.params["energyLoss"]
+            dvBounce = np.sqrt(np.abs(p.vel)) * self._params["energyLoss"]
             if bounce.any():
                 p.vel = np.where(p.vel > 0, p.vel - dvBounce ,p.vel + dvBounce)
-            p.vel *= self.params["friction"]
+            p.vel *= self._params["friction"]
 
-    def  move_hitter(self,action_vel):
-        self.hitter.pose = self.hitter.pose + self.Ts*self.params["maxVel"]*action_vel
+    def move_hitter(self,action_vel):
+        self.hitter.pose = self.hitter.pose + self.Ts*self._params["maxVel"]*action_vel
         self.hitter.vel = action_vel*self.in_bound(self.hitter)
         self.hitter.pose = np.clip(self.hitter.pose,
                                   a_min= self.hitter.radius,
-                                  a_max = self.bounds - self.hitter.radius )
-
-    def in_goal(self, pose):
-        return np.logical_and(pose >  self.goal_bounds[0],
-                              pose < np.add(self.goal_bounds[0],self.goal_bounds[1])).all()
-
+                                  a_max = self.bounds - self.hitter.radius)
+    def in_goal(self, poses):
+        print(poses[1::2]*self.bounds[1])
+        return np.logical_and((self.goal_bounds[0][0] <= poses[::2]*self.bounds[0]) &
+                              (poses[::2]*self.bounds[0] <= self.goal_bounds[0][0] + self.goal_bounds[0][1]),
+                              (self.goal_bounds[1][0] <= poses[1::2]*self.bounds[1]) &
+                              (poses[1::2]*self.bounds[1] <= self.goal_bounds[1][0] + self.goal_bounds[1][1])).all()
     def in_bound(self,puck):
         pose_in_bound = np.ones(shape=(2,),dtype = "bool")
         for xy, bound in enumerate(self.bounds):
             if puck.pose[xy] >= bound - puck.radius or puck.pose[xy] < puck.radius:
                 pose_in_bound[xy] = False
         return pose_in_bound
-    
+
     def _get_obs(self):
-        if self.params["obs_type"] == "rgb_array":
+        puck_poses = [pose for p in self.pucks for pose in (p.pose/self.bounds) ]
+        puck_vels  = [vel for p in self.pucks for vel in (p.vel)]
+        self._state = np.array([*(self.hitter.pose/self.bounds), 
+                                *puck_poses,
+                                *(self.hitter.vel),
+                                *puck_vels],dtype = "float32")
+
+        if self._params["obs_type"] == "rgb_array":
             return pygame.surfarray.array3d(self._canvas)
         else:
-            puck_poses = [pose for p in self.pucks for pose in (p.pose/self.bounds) ]
-            puck_vels  = [vel for p in self.pucks for vel in (p.vel)]
-            return np.array([*(self.hitter.pose/self.bounds), 
-                            *puck_poses,
-                            *(self.hitter.vel),
-                            *puck_vels],dtype = "float32")
-             
+            return self._state
+        
+    def reward(self,s0, action, s1):
+        reward = -.01
+        if self.in_goal(s1[2:(2+2*self._params["numPucks"])]).any():
+            reward += 1
+        print(reward)
+        return reward
+
+    def _get_info(self):
+        return {}
 
     def get_mouse_action(self):
         pygame.event.get()
@@ -150,10 +167,10 @@ class AirHockeyEnv(Env):
         dist = (mouse_position-self.hitter.pose)
         vel = dist/self.Ts
         vel_norm = np.linalg.norm(vel)
-        if  vel_norm > self.params["maxVel"]:
+        if  vel_norm > self._params["maxVel"]:
             vel = vel/vel_norm 
         else:
-            vel = vel/self.params["maxVel"]
+            vel = vel/self._params["maxVel"]
         return vel
 
     def update_puck_pose(self, puck):
@@ -166,13 +183,3 @@ class Puck:
         self.vel = np.zeros(shape = (2,))
         self.radius = np.array(radius)
         self.mass = np.array(mass)
-
-    def respawn(self, new_pose = None ):
-        if new_pose == None:
-            self.pose = self.init_pose
-        else:
-            self.pose = new_pose
-            self.init_pose  = new_pose
-    
-    def update_plot(self):
-        self.rect.move(self.pose[0],self.pose[1])
